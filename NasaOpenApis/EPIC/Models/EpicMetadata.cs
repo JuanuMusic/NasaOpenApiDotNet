052430910ba0db58f4c489684e0a7797e837cd4d
HarkDev <juanu@juanu.com.ar>
2019-04-07 13:09:08 -0300

    - Added Summaries Documentation

--- NasaOpenApis/EPIC/Models/EpicMetadata.cs
+++ NasaOpenApis/EPIC/Models/EpicMetadata.cs
@@ -3,18 +3,56 @@ using System;
 
 namespace NasaOpenApis.EPIC.Models
 {
-
+    /// <summary>
+    /// Represents the metadata received by the <see cref="EPICApi"/>.
+    /// </summary>
     public class EpicMetadata
     {
+        /// <summary>
+        /// Identifier for the image.
+        /// </summary>
         public string identifier { get; set; }
+
+        /// <summary>
+        /// Caption of the image.
+        /// </summary>
         public string caption { get; set; }
+
+        /// <summary>
+        /// The name of the image without the extension format.
+        /// </summary>
         public string image { get; set; }
+
         public string version { get; set; }
+
+        /// <summary>
+        /// Geographical coordinates that the satellite is looking at
+        /// </summary>
         public Coordinates centroid_coordinates { get; set; }
+        
+        /// <summary>
+        /// Position of the satellite in space
+        /// </summary>
         public Vector3 dscovr_j2000_position { get; set; }
+
+        /// <summary>
+        /// Position of the moon in space
+        /// </summary>
         public Vector3 lunar_j2000_position { get; set; }
+
+        /// <summary>
+        /// Position of the sun in space
+        /// </summary>
         public Vector3 sun_j2000_position { get; set; }
+
+        /// <summary>
+        /// Satellite attitude
+        /// </summary>
         public Quaternion attitude_quaternions { get; set; }
+
+        /// <summary>
+        /// The date this data was received.
+        /// </summary>
         public DateTime date { get; set; }
 
         /// <summary>
